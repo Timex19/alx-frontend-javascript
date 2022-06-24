7c7623bb0fb73a75b21731babb9da781be00ea68
Timex19 <ojooluwatimilehinpeter@yahoo.com>
2022-06-24 20:35:44 +0100

    task 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -42,6 +42,8 @@ console.log(printTeacher('john', 'doe'));
 interface StudentClassInterface {
     firstName: string;
     lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
 }
 
 interface StudentClassConstructorInterface {
